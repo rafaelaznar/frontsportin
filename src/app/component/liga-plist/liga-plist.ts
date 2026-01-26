70c4a37a5e193775ea91da8c0c87d323e50a2537
Olga Garcia <olgarciaq11@gmail.com>
2026-01-26 12:39:52 +0100

    crear liga-plist

---
+++ src/app/component/liga-plist/liga-plist.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liga-plist',
+  imports: [],
+  templateUrl: './liga-plist.html',
+  styleUrl: './liga-plist.css',
+})
+export class LigaPlist {
+
+}
