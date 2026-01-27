1fce66c811ab3c41a521c45d61665773d51ffafd
rond88 <r0nald19202006@gmail.com>
2026-01-27 10:33:43 +0100

    feat: add TipoarticuloPlist component with routing and basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './component/shared/home/home';
+import { TipoarticuloPlist } from './component/tipoarticulo-plist/tipoarticulo-plist';
 
 export const routes: Routes = [
     { path: '', component: Home },
+    { path: 'tipoarticulo', component: TipoarticuloPlist }
 ];
---
+++ src/app/component/tipoarticulo-plist/tipoarticulo-plist.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tipoarticulo-plist',
+  imports: [],
+  templateUrl: './tipoarticulo-plist.html',
+  styleUrl: './tipoarticulo-plist.css',
+})
+export class TipoarticuloPlist {
+
+}
