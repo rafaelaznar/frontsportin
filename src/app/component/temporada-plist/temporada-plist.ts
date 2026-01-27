7c922b9893e4d65cd7414edb941c3f8d6beb40ae
Lucia Castanera <castaneralucia@gmail.com>
2026-01-27 10:31:05 +0100

    temporada-plist.ts hecho

--- src/app/component/temporada-plist/temporada-plist.ts
+++ src/app/component/temporada-plist/temporada-plist.ts
@@ -1,24 +1,80 @@
 import { Component } from '@angular/core';
 
 import { RouterLink } from '@angular/router';
-import { Paginacion } from "../../shared/paginacion/paginacion";
-import { BotoneraRpp } from "../../shared/botonera-rpp/botonera-rpp";
-import { DatetimePipe } from "../../../pipe/datetime-pipe";
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { IPage, ITemporada } from '../../model/temporada';
+//import { IPage } from '../../../model/plist';
+import { TemporadaService } from '../../service/temporada';
+import { HttpErrorResponse } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-temporada-plist',
-  imports: [RouterLink, Paginacion, BotoneraRpp, DatetimePipe, MatDialogModule, MatSnackBarModule],
+  imports: [RouterLink],
   templateUrl: './temporada-plist.html',
   styleUrl: './temporada-plist.css',
 })
 export class TemporadaPlist {
 
-  oPage: IPage<IBlog> | null = null;
+  oPage: IPage<ITemporada> | null = null;
   numPage: number = 0;
   numRpp: number = 5;
+  totalElementsCount: number = 0;
+  rellenaOk: number | null = null;
+  rellenaError: string | null = null;
+
+
+  constructor(private oTemporadaService: TemporadaService) { }
+
+  oBotonera: string[] = [];
+  orderField: string = 'id';
+  orderDirection: string = 'asc';
+
+  ngOnInit() {
+    this.getPage();
+  }
+
+  getPage() {
+    this.oTemporadaService.getPage(this.numPage, this.numRpp, this.orderField, this.orderDirection).subscribe({
+      next: (data: IPage<ITemporada>) => {
+        this.oPage = data;
+        // actualizar contador actual
+        this.totalElementsCount = data.totalElements ?? 0;
+        this.rellenaOk = this.totalElementsCount;
+        // si estamos en una página que supera el límite entonces nos situamos en la ultima disponible
+        if (this.numPage > 0 && this.numPage >= data.totalPages) {
+          this.numPage = data.totalPages - 1;
+          this.getPage();
+        }
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error(error);
+      },
+    });
+  }
+
+  onOrder(order: string) {
+    if (this.orderField === order) {
+      this.orderDirection = this.orderDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.orderField = order;
+      this.orderDirection = 'asc';
+    }
+    this.numPage = 0;
+    this.getPage();
+    return false;
+  }
+
+  goToPage(numPage: number) {
+    this.numPage = numPage;
+    this.getPage();
+    return false;
+  }
+
+  onRppChange(n: number) {
+    this.numRpp = n;
+    this.getPage();
+    return false;
+  }
 
   
 }
--- src/app/service/temporada.ts
+++ src/app/service/temporada.ts
@@ -1,9 +1,25 @@
-
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { ITemporada, IPage } from "../model/temporada";
+import { serverURL } from '../environment/environment';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
+
 export class TemporadaService {
 
     constructor(private oHttp: HttpClient) { }
+
+    getPage(page: number, rpp: number, order: string = '', direction: string = ''): Observable<IPage<ITemporada>> {
+        if (order === '') {
+        order = 'id';
+        }
+        if (direction === '') {
+        direction = 'asc';
+        }
+        return this.oHttp.get<IPage<ITemporada>>(serverURL + `/temporada?page=${page}&size=${rpp}&sort=${order},${direction}`);
+    }
+
 }
