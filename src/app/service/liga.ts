40b6db58d6cf167affc43e1ea81547456a6741e6
Olga <olgarciaq11@gmail.com>
2026-01-27 21:08:38 +0100

    service hecho

---
+++ src/app/service/liga.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IPage } from '../model/plist';
+import { Observable } from 'rxjs';
+import { ILiga } from '../model/liga';
+import { serverURL } from '../environment/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class LigaService {
+    
+    constructor(private oHttp: HttpClient) { }
+
+    getPage(page: number, rpp: number, order: string = '', direction: string = ''): Observable<IPage<ILiga>> {
+        if (order === '') {
+            order = 'id';
+        }
+        if (direction === '') {
+            direction = 'asc';
+        }
+        return this.oHttp.get<IPage<ILiga>>(serverURL + `/liga?page=${page}&size=${rpp}&sort=${order},${direction}`);
+    }
+}
