7158e2385dfbe0e568fb010cf1fdb77912d1aa47
Ariel Sempertegui <arielsempertegu11@gmail.com>
2026-01-27 23:43:45 +0100

    Añadido getAll a tipousuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './component/shared/home/home';
+import { TipousuarioPlist } from './component/tipousuario/tipousuario-plist/tipousuario-plist';
 
 export const routes: Routes = [
     { path: '', component: Home },
+    { path: 'tipousuario/plist', component: TipousuarioPlist},
 ];
