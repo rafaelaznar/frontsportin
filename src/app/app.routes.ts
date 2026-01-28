96adb8f31ce3012038d58ab434009c8b8ddbef5c
Ariel Sempertegui <arielsempertegu11@gmail.com>
2026-01-28 19:50:28 +0100

    Añadido el getAll de Tipousuario | TipousuarioPlistAdminRouted

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './component/shared/home/home';
 import { ArticuloPlistAdminRouted } from './component/articulo/plist-admin-routed/articulo-plist';
+import { TipousuarioPlistAdminRouted } from './component/tipousario/plist-admin-routed/tipousuario-plist';
 
 export const routes: Routes = [
+    { path: 'tipousuario', component: TipousuarioPlistAdminRouted},
     { path: '', component: Home },
     { path: 'articulo', component: ArticuloPlistAdminRouted},
     { path: 'articulo?:id_tipoarticulo', component: ArticuloPlistAdminRouted} //pte
