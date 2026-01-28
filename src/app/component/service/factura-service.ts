410091eb2d089833922c54ffb00ca89ca81ab5ec
JoanSalinas <joansalinasf@gmail.com>
2026-01-28 07:42:25 +0100

    feat: implement factura-plist component with pagination and sorting functionality

---
+++ src/app/component/service/factura-service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { IFactura } from '../../model/factura';
+import { IPage } from '../../model/plist';
+import { serverURL } from '../../environment/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class FacturaService {
+
+
+  constructor(private oHttp: HttpClient) {}
+
+getPage(page: number, rpp: number, order: string = '', direction: string = ''): Observable<IPage<IFactura>> {
+    if (order === '') {
+      order = 'id';
+    }
+    if (direction === '') {
+      direction = 'asc';
+    }
+    return this.oHttp.get<IPage<IFactura>>(serverURL + `/factura?page=${page}&size=${rpp}&sort=${order},${direction}`);
+  }
+}
--- src/app/model/factura.ts
+++ src/app/model/factura.ts
@@ -1,17 +1,5 @@
-export interface IUsuario {
-   id: number;
-  nombre?: string;
-  apellido1?: string;
-  apellido2?: string;
-  username?: string;
-  password?: string;
-  [key: string]: any; 
-}
-
-
 export interface IFactura {
   id: number;
-  fecha: string;
-  usuarios : IUsuario;
-  
+  fecha: Date;
+  usuarios : number;
 }
