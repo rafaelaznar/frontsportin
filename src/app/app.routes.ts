36fe33abcf26a9e1074b2ce61796df2d0f30fdb1
JoanSalinas <joansalinasf@gmail.com>
2026-01-28 07:43:35 +0100

    feat: add route for factura-plist component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './component/shared/home/home';
+import { FacturaPlist } from './component/factura-plist/factura-plist';
+
 
 export const routes: Routes = [
     { path: '', component: Home },
+    { path: 'factura', component: FacturaPlist }
 ];
