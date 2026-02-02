170473959ae1e4888e2e7ef2783b1d83f2f4d62b
Iván <ivanx.afonso@gmail.com>
2026-02-03 00:03:05 +0100

    Agregar carrito y pequeña observacion de usuario

--- src/app/component/carrito/plist-admin-routed/carrito-plist.ts
+++ src/app/component/carrito/plist-admin-routed/carrito-plist.ts
@@ -76,8 +76,8 @@ export class CarritoPlistAdminRouted {
 
     this.carritoService
       .getPage(
-        this.numPage(),
-        this.numRpp(),
+        0, // siempre cargamos desde la primera página
+        1000, // lote grande para filtrar en cliente
         this.orderField(),
         this.orderDirection(),
         this.filtro().trim(),
