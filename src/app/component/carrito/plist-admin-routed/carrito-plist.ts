0700b8bfc1e18a431cd3a4fa6fe70023a965364c
Vlad Uski <sunfeax@gmail.com>
2026-02-03 17:26:38 +0100

    fix app.routes and carrito after bad merge

--- src/app/component/carrito/plist-admin-routed/carrito-plist.ts
+++ src/app/component/carrito/plist-admin-routed/carrito-plist.ts
@@ -76,8 +76,8 @@ export class CarritoPlistAdminRouted {
 
     this.carritoService
       .getPage(
-        0, // siempre cargamos desde la primera página
-        1000, // lote grande para filtrar en cliente
+        this.numPage(),
+        this.numRpp(),
         this.orderField(),
         this.orderDirection(),
         this.filtro().trim(),
