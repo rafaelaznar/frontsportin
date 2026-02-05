549ff335b69f2846c951adf28d2426b633888441
PollySOA <pollysoares.dev@gmail.com>
2026-02-05 11:59:16 +0100

    Add delete-admin-routed component with initial HTML, CSS, and TypeScript files

---
+++ src/app/component/comentario/delete-admin-routed/comentario-delete.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-admin-routed',
+  imports: [],
+  templateUrl: './comentario-delete.html',
+  styleUrls: ['./comentario-delete.css'],
+})
+export class DeleteAdminRouted {
+
+}
