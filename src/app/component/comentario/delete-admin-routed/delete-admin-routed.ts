3d9118c1c36b4682765af5af63ba17cad0dc44a6
PollySOA <pollysoares.dev@gmail.com>
2026-02-05 11:55:10 +0100

    Add delete-admin-routed and detail-admin-unrouted components with initial templates and styles

---
+++ src/app/component/comentario/delete-admin-routed/delete-admin-routed.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-admin-routed',
+  imports: [],
+  templateUrl: './delete-admin-routed.html',
+  styleUrl: './delete-admin-routed.css',
+})
+export class DeleteAdminRouted {
+
+}
---
+++ src/app/component/comentario/detail-admin-unrouted/comentario-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comentario-detail',
+  imports: [],
+  templateUrl: './comentario-detail.html',
+  styleUrls: ['./comentario-detail.css'],
+})
+export class DetailAdminUnrouted {
+
+}
