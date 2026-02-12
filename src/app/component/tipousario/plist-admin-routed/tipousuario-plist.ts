262ef87f9b1dc6e71ecef40b53931ee7f501685d
Alejandro Pavon <alejandropm06@gmail.com>
2026-02-12 08:25:45 +0100

    Tipousuario refactorizado

--- src/app/component/tipousario/plist-admin-routed/tipousuario-plist.ts
+++ src/app/component/tipousario/plist-admin-routed/tipousuario-plist.ts
@@ -1,70 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { HttpErrorResponse } from '@angular/common/http';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ITipousuario } from '../../../model/tipousuario';
-import { TipousuarioService } from '../../../service/tipousuario';
-import { RouterLink } from '@angular/router';
+import { PlistAdminUnrouted } from "../plist-admin-unrouted/tipousuario-plist";
 
 @Component({
   selector: 'app-tipousuario-plist',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, PlistAdminUnrouted],
   templateUrl: './tipousuario-plist.html',
   styleUrl: './tipousuario-plist.css',
 })
 export class TipousuarioPlistAdminRouted {
-
-  tipousuarios = signal<ITipousuario[]>([]);
-  // Variables de ordenamiento
-  orderField = signal<string>('id');
-  orderDirection = signal<'asc' | 'desc'>('asc');
-
-  constructor(private tipousuarioService: TipousuarioService) { }
-
-  ngOnInit() {
-    this.getAll();
-  }
-
-  getAll() {
-    this.tipousuarioService.getAll().subscribe({
-      next: (data: ITipousuario[]) => {
-        this.tipousuarios.set(data);
-        this.applyOrder();
-      },
-      error: (error: HttpErrorResponse) => {
-        console.error(error);
-      },
-    });
-  }
-
-  onOrder(column: string) {
-    if (this.orderField() === column) {
-      this.orderDirection.set(this.orderDirection() === 'asc' ? 'desc' : 'asc');
-    } else {
-      this.orderField.set(column);
-      this.orderDirection.set('asc');
-    }
-    this.applyOrder(); // Re-ordenamos el array localmente
-  }
-
-  private applyOrder() {
-    if (this.tipousuarios()) {
-      this.tipousuarios().sort((a, b) => {
-        let valueA = a[this.orderField() as keyof ITipousuario];
-        let valueB = b[this.orderField() as keyof ITipousuario];
-
-        if (typeof valueA === 'string') {
-          // Orden alfabético ignorando mayúsculas/minúsculas
-          return this.orderDirection() === 'asc' 
-            ? valueA.localeCompare(valueB as string)
-            : (valueB as string).localeCompare(valueA);
-        } else {
-          // Orden numérico (para el ID)
-          return this.orderDirection() === 'asc' 
-            ? (valueA as number) - (valueB as number)
-            : (valueB as number) - (valueA as number);
-        }
-      });
-    }
-  }
-
+  
 }
---
+++ src/app/component/tipousario/plist-admin-unrouted/tipousuario-plist.ts
@@ -0,0 +1,66 @@
+import { Component, inject, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ITipousuario } from '../../../model/tipousuario';
+import { HttpErrorResponse } from '@angular/common/http';
+import { TipousuarioService } from '../../../service/tipousuario';
+
+@Component({
+  selector: 'app-plist-admin-unrouted',
+  imports: [RouterLink],
+  templateUrl: './tipousuario-plist.html',
+  styleUrl: './tipousuario-plist.css',
+})
+export class PlistAdminUnrouted {
+  tipousuarios = signal<ITipousuario[]>([]);
+  // Variables de ordenamiento
+  orderField = signal<string>('id');
+  orderDirection = signal<'asc' | 'desc'>('asc');
+  oTipousuarioService = inject(TipousuarioService);
+
+  ngOnInit() {
+    this.getAll();
+  }
+
+  getAll() {
+    this.oTipousuarioService.getAll().subscribe({
+      next: (data: ITipousuario[]) => {
+        this.tipousuarios.set(data);
+        this.applyOrder();
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error(error);
+      },
+    });
+  }
+
+  onOrder(column: string) {
+    if (this.orderField() === column) {
+      this.orderDirection.set(this.orderDirection() === 'asc' ? 'desc' : 'asc');
+    } else {
+      this.orderField.set(column);
+      this.orderDirection.set('asc');
+    }
+    this.applyOrder(); // Re-ordenamos el array localmente
+  }
+
+  private applyOrder() {
+    if (this.tipousuarios()) {
+      this.tipousuarios().sort((a, b) => {
+        let valueA = a[this.orderField() as keyof ITipousuario];
+        let valueB = b[this.orderField() as keyof ITipousuario];
+
+        if (typeof valueA === 'string') {
+          // Orden alfabético ignorando mayúsculas/minúsculas
+          return this.orderDirection() === 'asc'
+            ? valueA.localeCompare(valueB as string)
+            : (valueB as string).localeCompare(valueA);
+        } else {
+          // Orden numérico (para el ID)
+          return this.orderDirection() === 'asc'
+            ? (valueA as number) - (valueB as number)
+            : (valueB as number) - (valueA as number);
+        }
+      });
+    }
+  }
+}
